ed7e19973ae64084cfbd36d4a2e128387269d0a5
Piotr Szkudlarek <szkudlarekp5@gmail.com>
2024-01-08 12:10:40 +0100

    Change db to postgresql

--- PlanktonDetector/PlanktonDetector/settings.py
+++ PlanktonDetector/PlanktonDetector/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "PlanktonDetector.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USERNAME"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
     }
 }
 
